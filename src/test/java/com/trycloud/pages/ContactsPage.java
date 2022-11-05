f26fee42f0b66ebe8021439d91690b576f64bc2a
Dmytro H <dmytrohashymov@gmail.com>
2022-11-05 15:29:46 -0400

    US 12. Finished.Ready for review. US 13 added and ready for review.

--- src/test/java/com/trycloud/pages/ContactsPage.java
+++ src/test/java/com/trycloud/pages/ContactsPage.java
@@ -1,6 +1,8 @@
 package com.trycloud.pages;
 
 import com.trycloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ContactsPage extends BasePage{
@@ -9,6 +11,7 @@ public class ContactsPage extends BasePage{
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-
+@FindBy(id = "contactsmenu-menu")
+    public WebElement contactsMenuPopup;
 
 }
