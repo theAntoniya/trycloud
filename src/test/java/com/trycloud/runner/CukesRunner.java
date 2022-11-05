edfceaa139d66802dfdf681df4959755d2f581b4
Umut <umut19950123@gmail.com>
2022-11-05 16:11:55 -0700

    US3 test is done

--- src/test/java/com/trycloud/runner/CukesRunner.java
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources",
         glue = "com/trycloud/step_definitions" ,
         dryRun = false ,
-        tags = ""
+        tags = "@us3"
 
 )
 public class CukesRunner {
